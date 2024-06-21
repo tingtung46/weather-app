193540bb79b9b5349376b5b2384d61e81b3c8813
root <132725436+tingtung46@users.noreply.github.com>
2024-06-21 07:35:39 +0700

    Configure eslint parser and gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import parseForESLint from '@babel/eslint-parser';
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'script',
+      parser: parseForESLint,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          presets: ['@babel/preset-env'],
+        },
+        sourceType: 'module',
+        allowImportExportEverywhere: true,
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
 ];
