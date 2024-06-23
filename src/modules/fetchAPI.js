ff83afe9b849cb4bb10ee824178395f0e0a65294
root <132725436+tingtung46@users.noreply.github.com>
2024-06-23 09:47:07 +0700

    Add condition of response

--- src/modules/fetchAPI.js
+++ src/modules/fetchAPI.js
@@ -1,21 +1,19 @@
 export const getWeather = async (weatherLocation) => {
   const apiKey = '04060da2460340b58b6113246241906';
-  const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${weatherLocation}&d=3`;
+  const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${weatherLocation}&days=3`;
 
   try {
     const response = await fetch(apiUrl, { mode: 'cors' });
 
-    if (!response.ok) {
-      throw new Error('Network failed');
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      const errorMsg = await response.json();
+      if (errorMsg.error.code === 1006) {
+        throw new Error('No location found');
+      }
     }
-
-    const data = await response.json();
-
-    if (data.error.code === 1006) {
-      throw new Error('Location not found');
-    }
-
-    return data;
   } catch (error) {
     console.error('Fetch failed', error);
   }
