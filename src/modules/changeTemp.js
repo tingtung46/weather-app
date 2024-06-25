f92a6acf167e356cb85af0fccf3d2ebf82058388
root <132725436+tingtung46@users.noreply.github.com>
2024-06-25 20:53:03 +0700

    Add toggle temp method

---
+++ src/modules/changeTemp.js
@@ -0,0 +1,39 @@
+export const changeTemp = (toggleBtn, celsiusDegree, fahrenheitDegree) => {
+  const temperatures = document.querySelectorAll('.temp');
+
+  temperatures.forEach((temp) => {
+    if (toggleBtn.checked) {
+      if (temp.textContent.includes(celsiusDegree)) {
+        if (temp.textContent.includes('Feels like')) {
+          const temperature = temp.textContent.split(' ')[2].slice(0, -1);
+          const newTemp = `Feels like ${convertToImperial(temperature)}${fahrenheitDegree}`;
+          temp.textContent = newTemp;
+        } else {
+          const temperature = temp.textContent.slice(0, -1);
+          const newTemp = `${convertToImperial(temperature)}${fahrenheitDegree}`;
+          temp.textContent = newTemp;
+        }
+      }
+    } else if (!toggleBtn.checked) {
+      if (temp.textContent.includes(fahrenheitDegree)) {
+        if (temp.textContent.includes('Feels like')) {
+          const temperature = temp.textContent.split(' ')[2].slice(0, -1);
+          const newTemp = `Feels like ${convertToMetric(temperature)}${celsiusDegree}`;
+          temp.textContent = newTemp;
+        } else {
+          const temperature = temp.textContent.slice(0, -1);
+          const newTemp = `${convertToMetric(temperature)}${celsiusDegree}`;
+          temp.textContent = newTemp;
+        }
+      }
+    }
+  });
+};
+
+const convertToMetric = (temp) => {
+  return Math.floor(((temp - 32) * 5) / 9);
+};
+
+const convertToImperial = (temp) => {
+  return Math.floor((temp * 9) / 5 + 32);
+};
