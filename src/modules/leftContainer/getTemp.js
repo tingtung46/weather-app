c3c969a6c75d21c0a03d6ecf47b4ac55e0ad1ef6
root <132725436+tingtung46@users.noreply.github.com>
2024-07-04 14:40:36 +0700

    Change span to p element

--- src/modules/leftContainer/getTemp.js
+++ src/modules/leftContainer/getTemp.js
@@ -9,11 +9,11 @@ export const createTempWrapper = (data, forecastData, celciusDegree) => {
   const minMaxTemp = document.createElement('div');
   minMaxTemp.classList.add('minmax-temp');
 
-  const minTemp = document.createElement('div');
+  const minTemp = document.createElement('p');
   minTemp.className = 'minmax temp';
   minTemp.textContent = `${Math.floor(forecastData[0].day.mintemp_c)}${celciusDegree}`;
 
-  const maxTemp = document.createElement('div');
+  const maxTemp = document.createElement('p');
   maxTemp.className = 'minmax temp';
   maxTemp.textContent = `${Math.floor(forecastData[0].day.maxtemp_c)}${celciusDegree}`;
 
