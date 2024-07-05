647ca217679735965af81607aea90e3d81285348
root <132725436+tingtung46@users.noreply.github.com>
2024-07-05 10:09:32 +0700

    Add scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,56 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { env } = require('process');
 
-const mode =
-  process.env.NODE_ENV === "production" ? "production" : "development";
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
- module.exports = {
-    mode: mode,
-    entry: './src/index.js',
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-        template: './src/index.html',
-        inject: 'body',
-      }),
+module.exports = {
+  mode: mode,
+  entry: './src/index.js',
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        assetModuleFilename: "[name][ext]",
-    },
-    module: {
-        rules: [
-            {
-            test: /\.(?:js|mjs|cjs)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', { targets: "defaults" }]
-                    ]
-                }
-            }
-            },
-            {
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-            },
-        ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new CssMinimizerPlugin()],
-        runtimeChunk: 'single',
-    },
- };
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+    runtimeChunk: 'single',
+  },
+};
